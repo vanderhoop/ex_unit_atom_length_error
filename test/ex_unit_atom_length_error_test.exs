bb4c9e5cbd63d977c9abb0d65ee1258eb55c2457
Travis Vander Hoop <vanderhoop@me.com>
2023-01-13 08:37:12 -0700

    Hmmm... this is the exact describe/test description that failed in the repo, yet it still passes in *this* reposotory. 🤔 What's the edge case I ran into then?

--- test/ex_unit_atom_length_error_test.exs
+++ test/ex_unit_atom_length_error_test.exs
@@ -1,12 +1,8 @@
 defmodule ExUnitAtomLengthErrorTest do
-  use ExUnit.Case
-  doctest ExUnitAtomLengthError
+  use ExUnit.Case, async: true
 
-  @one_hundred_and_fifty_char_string String.duplicate("a", 150)
-  @one_hundred_and_one_char_string String.duplicate("a", 101)
-
-  describe @one_hundred_and_fifty_char_string do
-    test @one_hundred_and_one_char_string do
+  describe "metrics_at_percentile_aggregated_by_url_and_date_grouped_by_path_and_date/0 when there's only one 'navigation' record" do
+    test "the result includes a single record with the ttfb/fcp/lcp values grouped by *date* (no time) of insertion for a single path" do
       assert true == false
     end
   end
