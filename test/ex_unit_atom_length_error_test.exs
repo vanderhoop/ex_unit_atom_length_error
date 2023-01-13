ef2f0e76d237e0d836dfef94fda624179e12fc55
Travis Vander Hoop <vanderhoop@me.com>
2023-01-13 15:18:13 -0700

    Okay, so pattern matching the test context reproduces the case, I suspect due to the generated atom adding the test's arity as /1. Error output below:
    Function: 'test metrics_at_percentile_aggregated_by_url_and_date_grouped_by_path_and_date/0 when there\'s only one \'navigation\' record the result includes a single record with the ttfb/fcp/lcp values grouped by *date* (no time) of insertion for a single path'/1
    
    == Compilation error in file test/ex_unit_atom_length_error_test.exs ==
    ** (CompileError) test/ex_unit_atom_length_error_test.exs: internal error in pass v3_kernel:
    exception error: a system limit has been reached
      in function  list_to_atom/1
         called as list_to_atom("-inlined-test metrics_at_percentile_aggregated_by_url_and_date_grouped_by_path_and_date/0 when there's only one 'navigation' record the result includes a single record with the ttfb/fcp/lcp values grouped by *date* (no time) of insertion for a single path/1-")
      in call from v3_kernel:translate_fc_args/4 (v3_kernel.erl, line 451)
      in call from v3_kernel:translate_match_fail/4 (v3_kernel.erl, line 419)
      in call from v3_kernel:match_guard_1/3 (v3_kernel.erl, line 1057)
      in call from v3_kernel:match_guard/3 (v3_kernel.erl, line 1050)
      in call from lists:foldr_1/3 (lists.erl, line 1372)
      in call from v3_kernel:expr/3 (v3_kernel.erl, line 317)
      in call from v3_kernel:expr/3 (v3_kernel.erl, line 285)
      in call from v3_kernel:function/2 (v3_kernel.erl, line 168)
      in call from lists:mapfoldl_1/3 (lists.erl, line 1462)
      in call from lists:mapfoldl_1/3 (lists.erl, line 1463)
      in call from v3_kernel:module/2 (v3_kernel.erl, line 134)
      in call from compile:v3_kernel/2 (compile.erl, line 1509)
      in call from compile:fold_comp/4 (compile.erl, line 405)
      in call from compile:internal_comp/5 (compile.erl, line 389)
      in call from compile:'-internal_fun/2-anonymous-0-'/2 (compile.erl, line 227)
      in call from elixir_erl_compiler:compile/3 (src/elixir_erl_compiler.erl, line 54)
      in call from elixir_erl:load_form/5 (src/elixir_erl.erl, line 439)
      in call from elixir_module:'-compile/6-fun-1-'/9 (src/elixir_module.erl, line 154)
      in call from elixir_erl_compiler:'-spawn/1-fun-0-'/2 (src/elixir_erl_compiler.erl, line 12)
        (stdlib 4.2) lists.erl:1442: :lists.foreach_1/2

--- test/ex_unit_atom_length_error_test.exs
+++ test/ex_unit_atom_length_error_test.exs
@@ -2,7 +2,7 @@ defmodule ExUnitAtomLengthErrorTest do
   use ExUnit.Case, async: true
 
   describe "metrics_at_percentile_aggregated_by_url_and_date_grouped_by_path_and_date/0 when there's only one 'navigation' record" do
-    test "the result includes a single record with the ttfb/fcp/lcp values grouped by *date* (no time) of insertion for a single path" do
+    test "the result includes a single record with the ttfb/fcp/lcp values grouped by *date* (no time) of insertion for a single path", %{} do
       assert true == false
     end
   end
