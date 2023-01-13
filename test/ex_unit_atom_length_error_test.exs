05ef8dfb982fab9871f44fdb1b6908d6bfb376ad
Travis Vander Hoop <vanderhoop@me.com>
2023-01-13 08:30:26 -0700

    Hmmmm... this gives me the *correct* error
      - I suspect the edge case I ran into was when 'test -' is prepended to the string
    
    (SystemLimitError) the computed name of a test (which includes its type, the name of its parent describe block if present, and the test name itself) must be shorter than 255 characters, got: "test this describe string and the test string below it are greater than 255 chars, especially when adding test this string is going to be more than 255 characters, and cause an atom length error when the below test fails, but it'll appears bizarrely to the user, as if the atom table is actually overrun it dkdkdkddkdkdkdkdkd"

--- test/ex_unit_atom_length_error_test.exs
+++ test/ex_unit_atom_length_error_test.exs
@@ -2,7 +2,9 @@ defmodule ExUnitAtomLengthErrorTest do
   use ExUnit.Case
   doctest ExUnitAtomLengthError
 
-  test "greets the world" do
-    assert ExUnitAtomLengthError.hello() == :world
+  describe "this describe string and the test string below it are greater than 255 chars, especially when adding test" do
+    test "this string is going to be more than 255 characters, and cause an atom length error when the below test fails, but it'll appears bizarrely to the user, as if the atom table is actually overrun it dkdkdkddkdkdkdkdkd" do
+      assert true == false
+    end
   end
 end
