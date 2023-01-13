cceeb5c9af24dc23e4ad1a3ef67520ab31130dc0
Travis Vander Hoop <vanderhoop@me.com>
2023-01-13 08:26:28 -0700

    mix new boilerplate

---
+++ lib/ex_unit_atom_length_error.ex
@@ -0,0 +1,18 @@
+defmodule ExUnitAtomLengthError do
+  @moduledoc """
+  Documentation for `ExUnitAtomLengthError`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExUnitAtomLengthError.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_unit_atom_length_error_test.exs
@@ -0,0 +1,8 @@
+defmodule ExUnitAtomLengthErrorTest do
+  use ExUnit.Case
+  doctest ExUnitAtomLengthError
+
+  test "greets the world" do
+    assert ExUnitAtomLengthError.hello() == :world
+  end
+end
